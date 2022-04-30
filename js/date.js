3a85154e1e596d0f8d95160285467ecfc60e72fe
albertoalmada <83609357+albertoalmada@users.noreply.github.com>
2022-04-30 19:18:33 -0300

    fix

--- js/date.js
+++ js/date.js
@@ -1,4 +1,3 @@
-document.querySelector('#lastUpdate').innerHTML=`&copy 2022 - Alberto Parreira Almada - Brazil -
-Last Update: ${document.lastModified}`;
+document.querySelector('#lastUpdate').innerHTML=`&copy 2022 - Alberto Parreira Almada - Brazil - Last Update: ${document.lastModified}`;
 
 
