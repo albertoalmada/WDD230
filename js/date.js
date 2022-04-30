e3e95e7eede809bab630a16c6910bc00953c3953
albertoalmada <83609357+albertoalmada@users.noreply.github.com>
2022-04-30 19:44:14 -0300

    trying

--- js/date.js
+++ js/date.js
@@ -1,1 +1,1 @@
-document.querySelector('#lastUpdate').innerHTML = `&copy - 2022 - Alberto Parreira Almada - Brazil - Last Update: ${document.lastModified}`;
+document.querySelector('#lastUpdate').innerHTML = `Last Update: ${document.lastModified}`;
