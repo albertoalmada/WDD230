7e07dc418efe29ffdd9a52fafaea7e410189fab2
albertoalmada <83609357+albertoalmada@users.noreply.github.com>
2022-04-30 13:48:20 -0300

    caahnges

--- js/date.js
+++ js/date.js
@@ -1,2 +1,2 @@
-document.querySelector('#lastUpDate').innerHTML = `&copy - 2022 - Alberto Parreira Almada - Brazil - 
+document.getByIdElement('#lastUpDate').innerHTML = `&copy - 2022 - Alberto Parreira Almada - Brazil - 
 Last Update: ${document.lastModified}`;
