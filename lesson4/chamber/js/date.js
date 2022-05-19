199653c3e2c577b8cb2d8cddbf58d1113398f658
albertoalmada <albertoalmada@yahoo.com>
2022-05-19 01:43:53 -0300

    W04 home page Chamber

---
+++ lesson4/chamber/js/date.js
@@ -0,0 +1,2 @@
+document.querySelector('#year').innerHTML = (new Date().getFullYear());
+document.querySelector('#lastUpdate').innerHTML = `Minersville Chamber of Commerce - Alberto Parreira Almada <br> Last Updated: ${document.lastModified}`;
