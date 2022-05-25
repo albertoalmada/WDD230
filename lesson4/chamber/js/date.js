3e728c333ec8ac989e9f27153886848d6418a368
albertoalmada <albertoalmada@yahoo.com>
2022-05-24 23:09:57 -0300

    corrections

--- lesson4/chamber/js/date.js
+++ lesson4/chamber/js/date.js
@@ -1,5 +1,9 @@
+document.getElementById('today').innerHTML = Date();
+
+
 document.querySelector('#year').innerHTML = (new Date().getFullYear());
-document.querySelector('#lastUpdate').innerHTML = `Minersville Chamber of Commerce - Alberto Parreira Almada <br> Last Updated: ${document.lastModified}`;
+document.querySelector('#lastUpdate').innerHTML = `Minersville Chamber of Commerce | Alberto Parreira Almada | 
+WDD 230 Project |<br> Last Updated: ${document.lastModified}`;
 
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
