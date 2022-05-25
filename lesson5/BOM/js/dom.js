3cb8c5063fc70d5c76cd143b2e2864c5c932b6de
albertoalmada <albertoalmada@yahoo.com>
2022-05-25 01:25:12 -0300

    dom activity

--- lesson5/BOM/js/dom.js
+++ lesson5/BOM/js/dom.js
@@ -1,3 +1,33 @@
+const input_user = document.querySelector("input");
+console.log(input_user);
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+button.addEventListener("click", () => {
+    const inputst = document.querySelector("input");
+    let storedInput = inputst.value;
+
+    console.log(storedInput);
+
+    document.querySelector("input").value = "";
+
+    const newListItem = document.createElement("li");
+    const newSpan = document.createElement("span");
+    const newButton = document.createElement("button");
+
+    newListItem.appendChild(newSpan);
+    newSpan.textContent = storedInput;
+    newListItem.appendChild(newButton);
+    newButton.textContent = "❌";
+    list.appendChild(newListItem);
+
+    newButton.addEventListener("click", () => {
+        list.removeChild(newListItem);
+    });
+    inputst.focus();
+
+});
+
+
 document.querySelector('#year').innerHTML = (new Date().getFullYear());
 document.querySelector('#lastUpdate').innerHTML = ` | DOM Activity | Alberto Almada | 
 WDD 230 W#05 | Last Updated: ${document.lastModified}`;
