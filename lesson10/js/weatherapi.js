53c47e7b2555daffb223958677274e679ced1301
albertoalmada <albertoalmada@yahoo.com>
2022-06-27 22:16:15 -0300

    weahter api first upload

---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,36 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weathericon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=SaoBernardodoCampo&units=metric&appid=9a7a792ce71b0657e7b7fb44bf7c2eca";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); 
+      
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(
+    0
+  )}</strong>`;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description.toUpperCase();
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = desc;
+}
